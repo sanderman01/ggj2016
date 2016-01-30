33e9f345d4abbee46f996f5e85bd350067239d94
Aldwoni(Pim Rutten) <aldwoni@gmail.com>
2016-01-30 14:29:52 +0100

    LevelManager

--- project/Assets/Scripts/LevelManager.cs
+++ project/Assets/Scripts/LevelManager.cs
@@ -1,18 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using Assets.Scripts.Model;
 using System;
 
 public class LevelManager : MonoBehaviour {
-	public Dictionary<GameObject, int> LevelPart =	new Dictionary<GameObject, int>();
-    public List<Challenge> ChallengeList = new List<Challenge>();
+    public const int levelLength = 100;
+	public List<LevelPart> levelParts =	new List<LevelPart>();
+    public List<Challenge> challengeList = new List<Challenge>();
+    public List<LevelPart> activeLevelParts = new List<LevelPart>();
 
     void Start()
     {
-        AddToChallengeList(LevelPart);
+        CreateLevel(levelParts);
+        AddToChallengeList(activeLevelParts);
     }
 
-    private void AddToChallengeList(Dictionary<GameObject, int> levelPart)
+    private void AddToChallengeList(List<LevelPart> activeLevelParts)
     {
         throw new NotImplementedException();
+    }
+
+    private void CreateLevel(List<LevelPart> levelParts)
+    {
+        List<LevelPart> temp = new List<LevelPart>();
+        int generated=0;
+        int count=0;
+        foreach(LevelPart lp in levelParts)
+        {
+            count += lp.chance;
+            for(int i=0;i<lp.chance;i++)
+            {
+                temp.Add(lp);
+            }
+        }
+        while(generated <levelLength)
+        {
+            int r = UnityEngine.Random.Range(0, count);
+            LevelPart newLevelPart = temp[r];
+            generated += newLevelPart.size;
+            activeLevelParts.Add(newLevelPart);
+        }
     }
 }
