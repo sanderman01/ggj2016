5c4cd981ed645013dc3e68ce272df0a946587315
VDZx <veadezwaan@yahoo.co.uk>
2016-01-30 13:20:17 +0100

    Created 'challenge' object

---
+++ project/Assets/Scripts/Challenge.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Challenge : MonoBehaviour {
+
+    public enum InputType
+    {
+        Jump
+    }
+
+    public InputType requiredInput = InputType.Jump; //Button to push/action to take to clear this challenge
+    public float timeLeftUntilInput = 9.5f; //Once this hits 0, you can start inputting the required command
+    public float timeLeftUntilJudgment = 10f; //Once this hits 0, you will fail the challenge unless it is already cleared
+    public bool cleared = false; //Set to true once the user enters the right input
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Clear()
+    {
+
+    }
+
+    public void Fail()
+    {
+
+    }
+}
